64d4e13889a684d83b15ba910998a37ab1955835
ssjf409 <35087350+ssjf409@users.noreply.github.com>
2018-01-11 12:05:28 +0900

    c

--- 05/moWebsocket.js
+++ 05/moWebsocket.js
@@ -1,7 +1,7 @@
 var g_mc_ws_component = {
     ws: null,
-    // url: 'wss://zoops-webrtc-01.herokuapp.com/echo',
-    url: 'ws://127.0.0.1:3001/signal',
+    url: 'wss://ssjf409-05-svr-express.herokuapp.com/signal',
+    // url: 'ws://127.0.0.1:3001/signal',
     init: function () {
 
     },
